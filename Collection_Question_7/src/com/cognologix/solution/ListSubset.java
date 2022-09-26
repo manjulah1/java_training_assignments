72adcc458991f8091aa9269cd0524f02d7a4cb46
manjulah <manjula.hiremath@cognologix.com>
2022-09-26 11:08:05 +0530

    refactored assignment 7

--- Collection_Question_7/src/com/cognologix/solution/ListSubset.java
+++ Collection_Question_7/src/com/cognologix/solution/ListSubset.java
@@ -3,6 +3,8 @@ package com.cognologix.solution;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.cognologix.controllers.SublistOperation;
+
 public class ListSubset {
 
 	public static void main(String[] args) {
@@ -10,41 +12,15 @@ public class ListSubset {
 		List<Integer> list2 = new ArrayList<Integer>();
 		int[] l1 = new int[] { 6, 7, 8, 9 };
 		int[] l2 = new int[] { 1, 2, 3, 4 };
-		list1 = addNumbersToList(l1);
-		list2 = addNumbersToList(l2);
+		
+		SublistOperation sublistOperation = new SublistOperation();
+		list1 = sublistOperation.addNumbersToList(l1);
+		list2 = sublistOperation.addNumbersToList(l2);
 
 		// check if list2 is a subset of list1
-		checkSubList(list1, list2);
-	}
-
-	private static void checkSubList(List<Integer> list1, List<Integer> list2) {
-		if (list1.size() > list2.size()) {
-			if (list1.containsAll(list2)) {
-				System.out.println("List1 is a superlist of List2");
-			} else {
-				System.out.println("List1 is not a superlist of List2");
-			}
-		} else if (list1.size() < list2.size()) {
-			if (list2.containsAll(list1)) {
-				System.out.println("List2 is a superlist of List1");
-			} else {
-				System.out.println("List2 is not a superlist of List1");
-			}
-		} else if (list1.size() == list2.size()) {
-			if (list1.containsAll(list2)) {
-				System.out.println("List1 is equal to List2");
-			} else {
-				System.out.println("List1 is not equal to List2");
-			}
-		}
-	}
-
-	private static List<Integer> addNumbersToList(int[] arr) {
-		List<Integer> list = new ArrayList<Integer>();
-		for (int i = 0; i < arr.length; i++) {
-			list.add(arr[i]);
-		}
-		return list;
+		String response = sublistOperation.checkSubList(list1, list2);
+		// print the result
+		System.out.println(response);
 	}
 
 }
