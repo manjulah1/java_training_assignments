965b80949e81e830379c083976469eac17ad4354
manjulah <manjula.hiremath@cognologix.com>
2022-09-26 12:25:20 +0530

    refactored assignment 9

--- Collection_Question_9/src/com/cognologix/solution/Anagram.java
+++ Collection_Question_9/src/com/cognologix/solution/Anagram.java
@@ -1,44 +1,26 @@
 package com.cognologix.solution;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
+
+import com.cognologix.operations.AnagramOperations;
 
 public class Anagram {
 	static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
 		String givenString;
-		List<String> candidatesList = new ArrayList<String>();
-		
 		System.out.println("Enter a string: ");
 		givenString = sc.nextLine();
+		givenString = givenString.toLowerCase();
 		
-		// get candidates list
-		candidatesList = getCandidates();
-		Iterator<String> it = candidatesList.iterator();
+		String[] candidateArray = { "enlists", "google", "inlets", "banana" };
+		List<String> candidateList = Arrays.asList(candidateArray);
 		
-		char[] givenArray = givenString.toCharArray();
-		Arrays.sort(givenArray);
-		while(it.hasNext()) {
-			String candidate = it.next();
-			char[] candidateArr = candidate.toCharArray();
-			Arrays.sort(candidateArr);			
-			boolean isAnagram = Arrays.equals(givenArray, candidateArr);
-			
-			if(isAnagram) {
-				System.out.println(candidate);
-			}
-		}
+		AnagramOperations anagramOperations = new AnagramOperations(candidateList, givenString);
+		
+		//check is Anagram
+		List<String> anagrams = anagramOperations.getAnagrams();
+		System.out.println(anagrams);
 	}
-	private static List<String> getCandidates() {
-		List<String> list = new ArrayList<String>();
-		list.add("enlists");
-		list.add("google");
-		list.add("inlets");
-		list.add("banana");
-		return list;
-	}
-
 }
