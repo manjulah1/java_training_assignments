1a4763e7bb30ef3c318148c8c77e0fde94039d81
manjulah <manjula.hiremath@cognologix.com>
2022-09-26 11:15:17 +0530

    refactored assignment 8

--- Collection_Question_8/src/com/cognologix/solution/IsogramEx.java
+++ Collection_Question_8/src/com/cognologix/solution/IsogramEx.java
@@ -1,38 +1,23 @@
 package com.cognologix.solution;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import com.cognologix.operations.IsogramOperations;
 
 public class IsogramEx {
-	static Scanner sc = new Scanner(System.in);
-
 	public static void main(String[] args) {
 		String inputString = null;
 
+		IsogramOperations isogramOperations = new IsogramOperations();
 		// take user input
 		System.out.println("Enter the string:");
-		inputString = userInput();
+		inputString = isogramOperations.userInput();
 
 		// check whether given input is an isogram
-		Boolean isIsogram = checkIsIsogram(inputString);
+		Boolean isIsogram = isogramOperations.checkIsIsogram(inputString);
 		if (isIsogram) {
 			System.out.println("The given input string is an Isogram");
 		} else {
 			System.out.println("The given input string is not an Isogram");
 		}
-	}
-
-	private static Boolean checkIsIsogram(String inputString) {
-		String[] arr = inputString.split(""); //toCharArray
-		Set<String> set = new HashSet<String>(Arrays.asList(arr));
-		return inputString.length() == set.size();
-	}
-
-	private static String userInput() {
-		String str = sc.nextLine();
-		return str;
 	}
 
 }
